b7b0ba60f84a5b93c995a4d93788ecc70d658e9a
JuanPFrancoL <juanp.franco@autonoma.edu.co>
2026-06-24 16:08:49 -0500

    Metodo eliminar al inicio

--- src/main/java/org/example/ListaDoble.java
+++ src/main/java/org/example/ListaDoble.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class ListaDoble {
     Nodo cabeza;
+    Nodo ultimo;
     int tamanio;
+    public int eliminarInicio() {
+        if(cabeza == null){
+            System.out.println("La lista esta vacia...");
+            return 0;
+        }
+        int valor = cabeza.valor;
+        cabeza = cabeza.siguiente;
+        tamanio--;
+        if (cabeza != null) {
+            cabeza.anterior = null;
+        }
+        else{
+            ultimo = null;
+        }
+        return valor;
+    }
 
     }
