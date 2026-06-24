591675591d2d999f9c5e270086de5fa51884639a
Karolay <luedk.garcia@autonoma.edu.co>
2026-06-24 15:42:52 -0500

    Se crea la estructura base del proyecto

---
+++ src/main/java/org/example/ListaDoble.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ListaDoble {
+    Nodo cabeza;
+    int tamanio;
+
+    }
---
+++ src/main/java/org/example/Nodo.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Nodo {
+    int valor;
+    Nodo siguiente;
+    Nodo anterior;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+    }
+}
