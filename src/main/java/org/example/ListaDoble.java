6a867103be2c051d7fc22a0598f8ee77c15011f2
Karolay <luedk.garcia@autonoma.edu.co>
2026-06-24 16:01:37 -0500

    Se crea el metodo de insertar al inicio

--- src/main/java/org/example/ListaDoble.java
+++ src/main/java/org/example/ListaDoble.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class ListaDoble {
     Nodo cabeza;
+    Nodo ultimo;
     int tamanio;
 
+    public ListaDoble() {
+        this.cabeza = null;
+        this.tamanio = 0;
     }
+
+
+    public void insertarAlInicio(int valor) {
+        Nodo nuevo = new Nodo(valor);
+        if (tamanio == 0) {
+            cabeza = nuevo;
+        }
+        else {
+            cabeza.anterior = nuevo;
+            nuevo.siguiente = cabeza;
+            cabeza = nuevo;
+        }
+        tamanio++;
+    }
+}
