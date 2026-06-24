32464431a93f5d021b6ce272fddb63af8263613e
Juandagomez <juand.gomezlo@autonoma.edu.co>
2026-06-24 16:18:25 -0500

    Metodo Imprimir

--- src/main/java/org/example/ListaDoble.java
+++ src/main/java/org/example/ListaDoble.java
@@ -4,4 +4,23 @@ public class ListaDoble {
     Nodo cabeza;
     int tamanio;
 
+    public static void MetodoImprimir(ListaDoble lista) {
+        if (lista.cabeza == null) {
+            System.out.println("La lista está vacía.");
+            return;
+        }
+
+        Nodo actual = lista.cabeza;
+
+        while (actual != null) {
+            System.out.print(actual.dato);
+
+            if (actual.siguiente != null) {
+                System.out.print(" <-> ");
+            }
+
+            actual = actual.siguiente;
+        }
+        System.out.println();
+    }
     }
