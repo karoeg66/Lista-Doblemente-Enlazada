531e753c09b1f623683653f3dff592738d2bc7d5
Juandagomez <juand.gomezlo@autonoma.edu.co>
2026-06-26 08:41:13 -0500

    Metodo isEmpty

--- src/main/java/org/example/ListaDoble.java
+++ src/main/java/org/example/ListaDoble.java
@@ -4,15 +4,8 @@ public class ListaDoble {
     Nodo cabeza;
     int tamanio;
 
-    public int size(){
-        int contador = 0;
-        Nodo actual = cabeza;
-
-        while(actual != null){
-            contador++;
-            actual = actual.siguiente;
-        }
-        return contador;
+    public boolean isEmpty(){
+        return cabeza == null;
     }
 
 
